016a41a0ae07bcda5c920d1c493b49bcfe6cbdbd
RaiseOD <75505270+RaiseOD@users.noreply.github.com>
2021-09-13 14:21:33 -0700

    Python

---
+++ main.py
@@ -0,0 +1,13 @@
+# Ryan von Lutzow
+# TicTacToeGUI Main
+# 9-13-21
+
+import tkinter
+
+root = tkinter.Tk()
+
+root.title("TicTacToeGUI")
+
+tkinter.Button(text="Click", width=20, height=10, fg="orange", bg="black").pack()
+
+root.mainloop()
