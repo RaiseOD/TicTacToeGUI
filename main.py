6d83d43b0c7cb2ffaef8a37ca1782abc46952dbf
RaiseOD <75505270+RaiseOD@users.noreply.github.com>
2021-09-13 14:26:15 -0700

    Printing works with lambda

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ root = tkinter.Tk()
 
 root.title("TicTacToeGUI")
 
-tkinter.Button(text="Click", width=20, height=10, fg="orange", bg="black").pack()
+tkinter.Button(text="Click", command=lambda:print("Click!"), width=20, height=10, fg="orange", bg="black").pack()
 
 root.mainloop()
